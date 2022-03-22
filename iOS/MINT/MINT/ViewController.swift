0aace083ec8b70cde6712da7820ee33e17663f5d
조용문 <choymoonk@gmail.com>
2022-03-22 11:36:49 +0900

    Init iOS Project

---
+++ iOS/MINT/MINT/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MINT
+//
+//  Created by choymoon on 2022/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
